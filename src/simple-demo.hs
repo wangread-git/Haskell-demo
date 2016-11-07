1dc0a0bd6c66626764e20f280552761ba325885b
bjyfwangrui <yfwangrui@jd.com>
2016-11-07 12:23:07 +0800

    Syntax in Function

--- src/simple-demo.hs
+++ src/simple-demo.hs
@@ -1,2 +1,53 @@
 addTwo :: Int -> Int -> Int
 addTwo x y = x + y
+
+lucky :: (Integral a) => a -> String
+lucky 1 = "One"
+lucky 2 = "Two"
+lucky 3 = "Three"
+lucky 4 = "Four"
+lucky 5 = "Five"
+lucky x = "Not between 1 and 5"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial(n - 1)
+
+--addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+--addVectors a b = (fst a + fst b, snd a + snd b)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+mySum :: (Num a) => [a] -> a
+mySum [] = 0
+mySum (x:xs) = x + mySum xs
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | otherwise                   = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b     = GT
+    | a == b    = EQ
+    | otherwise = LT
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
